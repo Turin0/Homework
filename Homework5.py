71a2e9b24b93958729f0a2608c28c4adf6565d42
Turin0 <163298286+Turin0@users.noreply.github.com>
2024-03-20 12:05:40 +0300

    Homework5.py

--- Homework5.py
+++ Homework5.py
@@ -1,11 +1,11 @@
-my_list = ['Apple', 'Orange', 'Peach', 'Mellon', 'Potato']
+my_list = ['Apple', 'Orange', 'Peach', 'Melon', 'Potato']
 print(my_list)
 print(my_list[0])
 print(my_list[-1])
 print(my_list[2:5])
 my_list[2] = 'Banana'
 print(my_list)
-my_dict = {'Apple': 'Яблоко', 'Orange': 'Апельсин', 'Peach': 'Персик', 'Mellon': 'Дыня', 'Potato': 'Картошка'}
+my_dict = {'Apple': 'Яблоко', 'Orange': 'Апельсин', 'Peach': 'Персик', 'Melon': 'Дыня', 'Potato': 'Картошка'}
 print(my_dict)
 print(my_dict['Peach'])
 my_dict['Kiwi'] = 'Киви'
