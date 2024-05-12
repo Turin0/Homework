175fd1355e5b69dc0edb0d0e455e96fcbc5c09e1
Turin0 <163298286+Turin0@users.noreply.github.com>
2024-05-12 18:26:24 +0300

    Add files via upload

---
+++ Homework_7.2.py
@@ -0,0 +1,15 @@
+file_name = 'Poem.txt'
+file = open(file_name, mode='w', encoding='utf8')
+file_content = ("# -*- coding: utf-8 -*- \nMy soul is dark - Oh! quickly string \nThe harp I yet can brook to hear; \n"
+                "And let thy gentle fingers fling \nIts melting murmurs o'er mine ear. \n"
+                "If in this heart a hope be dear, \nThat sound shall charm it forth again: \n"
+                "If in these eyes there lurk a tear, \n'Twill flow, and cease to burn my brain. \n"
+                "\nBut bid the strain be wild and deep, \nNor let thy notes of joy be first: \n"
+                "I tell thee, minstrel, I must weep, \nOr else this heavy heart will burst; \n"
+                "For it hath been by sorrow nursed, \nAnd ached in sleepless silence, long; \n"
+                "And now 'tis doomed to know the worst, \nAnd break at once - or yield to song.")
+file.write(file_content)
+file.close()
+with open(file_name, mode='r', encoding='utf8') as file: # Оператор with позволяет контролировать вход и выход из файла
+    for line in file:
+        print(line, end="")
