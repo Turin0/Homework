187532d936bc8be13af79feb9cb3f1df2423f869
Turin0 <163298286+Turin0@users.noreply.github.com>
2024-05-19 11:39:44 +0300

    Add files via upload

---
+++ Homework_8.1.py
@@ -0,0 +1,12 @@
+def add_everything_up(a, b):
+    try:
+        print(a + b)
+    except TypeError:
+        print(str(a)+str(b))
+    else:
+        print(a+b)
+
+
+add_everything_up(123.456, 'строка')
+add_everything_up('яблоко', 4215)
+add_everything_up(123.456, 7)
