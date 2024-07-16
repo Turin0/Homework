5c7051f32d6bf064424352ed199c00404c0515c0
Turin0 <163298286+Turin0@users.noreply.github.com>
2024-07-16 16:56:10 +0300

    Add files via upload

---
+++ Test_Tournament.py
@@ -0,0 +1,44 @@
+import unittest
+import runner
+
+
+class TournamentTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.runner_1 = runner.Runner(name='Усейн', speed=10)
+        self.runner_2 = runner.Runner(name='Андрей', speed=9)
+        self.runner_3 = runner.Runner(name='Ник', speed=3)
+        self.t_list_1 = [self.runner_1, self.runner_3]
+        self.t_list_2 = [self.runner_2, self.runner_3]
+        self.t_list_3 = [self.runner_1, self.runner_2, self.runner_3]
+
+    @classmethod
+    def tearDownClass(cls):
+        print(cls.all_results[1])
+        print(cls.all_results[2])
+        print(cls.all_results[3])
+
+    def test_t1(self):
+        t1 = runner.Tournament(90, *self.t_list_1)
+        t1.start()
+        result = t1.finishers
+        self.assertEqual(result[2], 'Ник')
+        self.all_results[1] = result
+
+    def test_t2(self):
+        t2 = runner.Tournament(90, *self.t_list_2)
+        t2.start()
+        result = t2.finishers
+        self.assertEqual(result[2], 'Ник')
+        self.all_results[2] = result
+
+    def test_t3(self):
+        t3 = runner.Tournament(90, *self.t_list_3)
+        t3.start()
+        result = t3.finishers
+        self.assertEqual(result[3], 'Ник')
+        self.all_results[3] = result
