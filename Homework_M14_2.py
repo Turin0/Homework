526881f8ae4260180b3e85fe8df1594a7ab6e270
Turin0 <163298286+Turin0@users.noreply.github.com>
2024-08-05 17:13:24 +0300

    Add files via upload

---
+++ Homework_M14_2.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
+# for i in range(1, 11):
+#     cursor.execute('INSERT INTO Users(username, email, age ,balance) VALUES(?,?,?,?)',
+#                    (f'User{i}', f'example{i}@gmail.com', f'{i*10}', '1000'))
+#
+# for i in range(1, 11, 2):
+#     cursor.execute('UPDATE Users SET balance = ? WHERE id = ?', ('500', f'{i}'))
+#
+# for i in range(1, 11, 3):
+#     cursor.execute('DELETE FROM Users WHERE id = ?', (f'{i}',))
+cursor.execute('DELETE FROM Users WHERE id = ?', ('6',))
+cursor.execute('SELECT COUNT(*) FROM Users')
+all_users = cursor.fetchone()[0]
+print(all_users)
+cursor.execute('SELECT SUM(balance) FROM Users')
+all_balance = cursor.fetchone()[0]
+print(all_balance)
+print(all_balance/all_users)
+cursor.execute('SELECT AVG(balance) FROM Users')
+balance_avg = cursor.fetchone()[0]
+print(balance_avg)
+connection.commit()
+connection.close()
